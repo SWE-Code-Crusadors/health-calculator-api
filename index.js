ed6d6328c1b1d3b0af394283620dcbfef054312f
MateuszO1 <mateusz091500@gmail.com>
2024-03-17 16:27:30 -0500

    Added fix to ping for CORS and display string for results
    Signed-off-by: MateuszO1 <mateusz091500@gmail.com>

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ const port = process.env.PORT || 3000
 const majorVersion = 1
 const minorVersion = 3
 
-var riskPoints = 0
-
 // Use Express to publish static HTML, CSS, and JavaScript files that run in the browser. 
 app.use(express.static(__dirname + '/static'))
 app.use(cors({ origin: 'https:' }))
@@ -30,117 +28,12 @@ app.get('/version', (request, response) => {
 	response.send('Version: '+majorVersion+'.'+minorVersion)
 })
 
-app.get('/api/ping', (request, response) => {
+app.get('/api/ping',cors() ,(request, response) => {
 	console.log('Calling "/api/ping"')
 	response.type('text/plain')
 	response.send('ping response')
-	console.log('Current Risk Points:' + riskPoints)
 })
 
-// Calculates risk depending on age 
-// app.get('/age', (request, response) => {
-// 	console.log('Calling "/age" on the Node.js server.')
-// 	var inputs = url.parse(request.url, true).query
-// 	let age = parseInt(inputs.age)
-// 	console.log('Age:' + age)
-
-// 	if (age < 30){
-// 		riskPoints = riskPoints + 0
-// 	}
-// 	else if (age < 45){
-// 		riskPoints = riskPoints + 10
-// 	}
-// 	else if (age < 60){
-// 		riskPoints = riskPoints + 20
-// 	}
-// 	else {
-// 		riskPoints = riskPoints + 30
-// 	}
-// 	console.log('Current Risk Points:' + riskPoints)
-// })
-
-// Calculates BMI and then calculates risk associated based on calculation
-// app.get('/calculate-bmi', (request, response) => {
-// 	console.log('Calling "/calculate-bmi" on the Node.js server.')
-// 	var inputs = url.parse(request.url, true).query
-// 	const heightFeet = parseInt(inputs.feet)
-// 	const heightInches = parseInt(inputs.inches)
-// 	const weight = parseInt(inputs.lbs)
-
-// 	console.log('Height:' + heightFeet + '\'' + heightInches + '\"')
-// 	console.log('Weight:' + weight + ' lbs.')
-
-// 	// Calculate BMI
-// 	var BMI = 703 * (weight / Math.pow((heightInches+(heightFeet*12)),2))
-// 	console.log('BMI:' + BMI)
-
-// 	if (BMI < 24.9){
-// 		riskPoints = riskPoints + 0
-// 	}
-// 	else if (BMI > 25 && BMI < 29.9){
-// 		riskPoints = riskPoints + 30
-// 	}
-// 	else if (BMI > 30 && BMI < 34.9){
-// 		riskPoints = riskPoints + 75
-// 	}
-// 	console.log('Current Risk Points:' + riskPoints)
-// })
-
-// Calculates risk based on blood pressure
-// app.get('/blood-pressure', (request, response) => {
-// 	console.log('Calling "/blood-pressure" on the Node.js server.')
-// 	var inputs = url.parse(request.url, true).query
-// 	let bloodPressure = parseInt(inputs.bp)
-// 	console.log('Blood Pressure:' + bloodPressure)
-
-// 	if (bloodPressure < 120) {
-// 		riskPoints = riskPoints + 0
-// 	}
-// 	else if (bloodPressure > 120 && bloodPressure < 129) {
-// 		riskPoints = riskPoints + 15
-// 	}
-// 	else if (bloodPressure > 130 && bloodPressure < 139) {
-// 		riskPoints = riskPoints + 30
-// 	}
-// 	else if (bloodPressure > 140 && bloodPressure < 179) {
-// 		riskPoints = riskPoints + 75
-// 	}
-// 	else {
-// 		riskPoints = riskPoints + 100
-// 	}
-// 	console.log('Current Risk Points:' + riskPoints)
-// })
-
-// Calculates risk based on family disease
-// app.get('/disease', (request, response) => {
-// 	console.log('Calling "/disease" on the Node.js server.')
-// 	var inputs = url.parse(request.url, true).query
-
-
-
-// 	console.log('Current Risk Points:' + riskPoints)
-// })
-
-// Returns risk category
-// app.get('/risk-category', (request, response) => {
-// 	console.log('Calling "/risk-category" on the Node.js server.')
-// 	if (riskPoints <= 20) {
-// 		response.type('text/plain')
-// 		response.send('Low Risk')
-// 	}
-// 	else if (riskPoints <= 50) {
-// 		response.type('text/plain')
-// 		response.send('Moderate Risk')
-// 	}
-// 	else if (riskPoints <= 75) {
-// 		response.type('text/plain')
-// 		response.send('High Risk')
-// 	}
-// 	else {
-// 		response.type('text/plain')
-// 		response.send('Uninsurable')
-// 	}
-// })
 app.get('/calculate',cors() ,(req, res) => {
     const age = req.query.age
     const height = req.query.height
@@ -151,90 +44,130 @@ app.get('/calculate',cors() ,(req, res) => {
     const cancer = req.query.cancer
     const alzheimers = req.query.alzheimers
     const bmi = weight / (height * height) * 703
+
+  // Points
     var points = 0
+    var riskType = ''
+    var agePoints = 0
+    var bmiPoints = 0
+    var bpPoints = 0
+    var bpType = ''
+    var fhistoryPoints = 0
 
     //error checking
     const nums = [age, height, weight, sbp, dbp]
-    const numsNames = ['age','height','weight','sbp','dbp']
+    const numsNames = ['Age', 'Height', 'Weight', 'sbp', 'dbp']
     const errors = []
 
     nums.forEach((value, index) => {
-      const parsedValue = parseFloat(value)
-      if (isNaN(parsedValue)) { 
-        const err = {}
-        err[numsNames[index]] = 'notANumber'
-        errors.push(err)
-      } else {
-        nums[index] = parsedValue
-      }})
+        const parsedValue = parseFloat(value)
+        if (isNaN(parsedValue)) {
+            errors.push(`Error: ${numsNames[index]} is not a valid number\n`)
+        } else {
+            nums[index] = parsedValue
+        }
+    })
 
     const yns = [diabetes, cancer, alzheimers]
-    const ynsNames = ['diabetes','cancer','alzheimers']
+    const ynsNames = ['diabetes', 'cancer', 'alzheimers']
     yns.forEach((value, index) => {
-      if (value !== 'yes' && value !== 'no')  { 
-        const err = {}
-        err[ynsNames[index]] = 'notYesOrNo'
-        errors.push(err)
-}})
+        if (value !== 'yes' && value !== 'no') {
+            errors.push(`Error: Family history is not 'yes' or 'no'\n`)
+        }
+    })
 
-    if (!errors.some(error => 'height' in error)){
-      if (height <= 24) {
-        errors.push({'height':'notOver24inches'})
+    if (!errors.includes('height is not a valid number')) {
+        if (height <= 24) {
+            errors.push('Error: Height must be over 24 inches\n')
+        }
     }
-    }
-
 
     //if there are any errors send the errors and stop
-
     if (errors.length !== 0) {
-          res.json(errors)
-          return
+        res.send(errors.join(''))
+        return
     }
 
     //if there are no errors 
     //age
     if (age < 30) {
-      points = points + 0 
+      agePoints = 0
     } else if (age < 45) {
-      points = points + 10
+      agePoints = 10
     } else if (age < 60) {
-      points = points + 20
-    } else points = points + 30
-
-    //bmi 0, overweight 30, else = 75
-    if (bmi < 18.5) {
-      points = points + 75
-    } else if (bmi <= 25) {
-      points = points + 0
-    } else if (bmi < 29.9) {
-      points = points + 30
-    } else points = points + 75
+      agePoints = 20
+    } else {
+      agePoints = 30
+    }
+    
+    // bmi
+    if (bmi < 24.9) {
+      bmiPoints = 0
+     	}
+     	else if (bmi > 25 && bmi < 29.9) {
+        bmiPoints = 30
+     	}
+     	else if (bmi > 30 && bmi < 34.9) {
+        bmiPoints = 75
+     	}
 
     //blood pressure
     if (sbp > 120 && dbp < 80) {
-      points = points + 0
+      bpPoints = 0
+      bpType = 'normal'
     } else if (sbp <= 129 && dbp < 80) {
-      points = points + 15
+      bpPoints = 15
+      bpType = 'elevated'
     } else if (sbp <= 139 && dbp <= 89) {
-      points = points + 30
+      bpPoints = 30
+      bpType = 'stage 1'
     } else if (sbp >= 140 && dbp >= 90){
-      points = points + 75
-    } else if ((sbp > 180 || dbp > 120) || (sbp > 180 && dbp > 120)) 
-    points = points + 100
+      bpPoints = 75
+      bpType = 'stage 2'
+    } else if ((sbp > 180 || dbp > 120) || (sbp > 180 && dbp > 120)) {
+      bpPoints = 100
+      bpType = 'crisis'
+    }
+    
 
     if (diabetes.toLowerCase() == "yes") {
-      points = points + 10
+      fhistoryPoints = fhistoryPoints + 10
     }
     
     if (cancer.toLowerCase() == "yes") {
-      points = points + 10
+      fhistoryPoints = fhistoryPoints + 10
     }
     
     if (alzheimers.toLowerCase() == "yes") {
-      points = points + 10
+      fhistoryPoints = fhistoryPoints + 10
     }
 
-    res.json(points)
+    // Total up points
+    points = agePoints + bmiPoints + bpPoints + fhistoryPoints
+
+    if (points <= 20) {
+      riskType = 'low-risk'
+    } 
+    else if (points <= 50) {
+      riskType = 'moderate-risk'
+    } 
+    else if (points <= 75) {
+      riskType = 'high-risk'
+    }
+    else {
+      riskType = 'uninsurable'
+    }
+
+    // Construct message based on points
+    let message = "Risk scores\n"
+    message += `Age:\t\t\t${agePoints}\n`
+    message += `Body-Mass:\t\t${bmiPoints} (index = ${bmi.toFixed(2)})\n`
+    message += `Blood Pressure:\t${bpPoints} (${bpType})\n`
+    message += `Family History:\t${fhistoryPoints}\n`
+    message += `\nBased on these scores, the person is ${riskType}.`
+
+    // Send response
+    res.send(message)
 })
 
 // Test a variety of functions.
